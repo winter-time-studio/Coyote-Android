9ad0effc06c61faa6faf2ca5cac5c74e594b1ea7
Tecco <glaypppgod56d@gmail.com>
2015-10-10 18:30:27 +0900

    ActionBarにimage設定、Settingアイコンを変更

--- app/src/main/java/com/winter_time_studio/coyote/MainActivity.java
+++ app/src/main/java/com/winter_time_studio/coyote/MainActivity.java
@@ -1,9 +1,12 @@
 package com.winter_time_studio.coyote;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,13 +14,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // ActionBarの設定
+        if (savedInstanceState == null) {
+            // customActionBarの取得
+            View customActionBarView = this.getActionBarView("Hoge", "表示する画像のURL");
+
+            // ActionBarの取得
+            ActionBar actionBar = this.getSupportActionBar();
+
+            // タイトルを表示するか
+            actionBar.setDisplayShowTitleEnabled(true);
+            // iconを表示するか
+            actionBar.setDisplayShowHomeEnabled(false);
+
+            actionBar.setCustomView(customActionBarView);
+            actionBar.setDisplayShowCustomEnabled(true);
+        }
     }
 
+
+    private View getActionBarView(String title, String imageURL) {
+
+        // 表示するlayoutファイルの取得
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View view = inflater.inflate(R.layout.custom_action_bar, null);
+
+        // CustomViewにクリックイベントを登録する
+        final MainActivity self = this;
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                self.finish();
+            }
+        });
+        return view;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
         return true;
     }
 
